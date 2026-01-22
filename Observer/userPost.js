7cef5fa8a3c8061159247d1438318d5708775dd0
Joryi Vazquez <joryi.vazquez@roo.vet>
2026-01-22 16:42:28 -0600

    chore: adjusting the function to receive a user

--- Observer/userPost.js
+++ Observer/userPost.js
@@ -1,10 +1,10 @@
 const notifier = require('./notifier');
 
-function createPost(post) {
-    console.log(`Creating post ${post.title}...`);
-    notifier.emit('postCreated', post);
+function createPost(user, post) {
+    console.log(`Creating post ${post.title}... for user ${user.name}`);
+    notifier.emit('postCreated', user, post);
     // implement create post logic here
-    return post;
+    return { user, post };
 }
 
 module.exports = createPost;
