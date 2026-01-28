46582960049c070b2ec42ba457b8dac6c0fd794c
Joryi Michel Vazquez Perez <jperez@dotdashmdp.com>
2026-01-27 21:39:18 -0600

    chore: adding another middleware to be executed depending on middleware 3

--- middleware/index.js
+++ middleware/index.js
@@ -16,15 +16,30 @@ const middleware1 = (req, res, next) => {
 
 const middleware2 = (req, res, next) => {
     console.log('middleware2 : processing request');
+    let httpStatus = Math.floor(Math.random() * 500) + 100;
+    let validCode = false;
+    if (httpStatus >= 200 && httpStatus < 300) {
+        validCode = true;
+    } else {
+        validCode = false;
+    }
+    res.validCode = validCode;
     next();
 }
 
 const middleware3 = (req, res, next) => {
     console.log('middleware3 : finalizing request');
+    if (res.validCode) {
+        next();
+    } 
+}
+
+const middleware4 = (req, res, next) => {
+    console.log('middleware4 : sending response');
     next();
 }
 
 const req ={}
-const res ={}
-const middlewares = [middleware1, middleware2, middleware3];
+const res ={ validCode: false };
+const middlewares = [middleware1, middleware2, middleware3, middleware4];
 runMiddleware(req, res, middlewares);
