75646065f330fac15df20bc65e6900638d010f6c
Joryi Vazquez <70546473+joryimazterzon@users.noreply.github.com>
2026-02-17 18:34:43 -0600

    chore: adding pino to print logs (#11)
    Co-authored-by: Joryi Michel Vazquez Perez <jperez@dotdashmdp.com>

--- logger/index.js
+++ logger/index.js
@@ -1,6 +1,39 @@
+const pino = require('pino');
+const isProduction = process.env.NODE_ENV === 'production';
+
+const developmentConfig = {
+  level: 'debug',
+  transport: {
+    target: 'pino-pretty', // para que se vea bonito en la consola
+    options: {
+      colorize: true, // para que se vea bonito en la consola
+      ignore: 'pid,hostname', // para que no se vea el pid y el hostname
+      translateTime: 'HH:MM:ss', // para que se vea  hora en el formato dd-mm-yyyy HH:MM:ss
+    },
+  },
+};
+
+const productionConfig = {
+  level: 'info',
+  transport: {
+    target: 'pino/file',
+    options: {
+      destination: 'app.log', // archivo de log
+      translateTime: 'dd-mm-yyyy HH:MM:ss Z', // formato de la fecha y hora
+      levelFirst: true,
+    },
+  },
+};
+
+const config = isProduction ? productionConfig : developmentConfig;
+const logger = pino(config);
+
+logger.info('Logger initialized');
+
+let k = 0;
 function operation() {
   setInterval(() => {
-    console.log('Operation running');
+    logger.debug({ message: 'Operation running', contador: k++ });
   }, 1000);
   //   throw new Error('Operation failed');
 }
@@ -8,12 +41,12 @@ function operation() {
 operation();
 
 process.on('uncaughtException', (err) => {
-  console.error('Uncaught exception:', err);
+  logger.error('Uncaught exception:', err);
   process.exit(1);
 });
 
 process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled rejection:', reason);
+  logger.error('Unhandled rejection:', reason);
   process.exit(1);
 });
 
@@ -21,7 +54,26 @@ const signals = ['SIGINT', 'SIGTERM', 'SIGQUIT', 'SIGBREAK'];
 
 signals.forEach((signal) => {
   process.on(signal, () => {
-    console.log(`${signal} received`);
+    logger.info(`${signal} received`);
     process.exit(0);
   });
 });
+
+/**
+ * Pino es un logger muy popular en Node.js.
+ * node index.js
+ * node index.js --log-level=debug
+ * node index.js --log-level=info
+ * node index.js --log-level=warn
+ * node index.js --log-level=error
+ * node index.js --log-level=fatal
+ * node index.js --log-level=trace
+ * node index.js --log-level=silent
+ * node index.js --log-level=debug
+ * node index.js --log-level=info
+ * node index.js --log-level=warn
+ *
+ * NODE_ENV=production node index.js
+ * No muestra nada en la consola
+ *
+ */
